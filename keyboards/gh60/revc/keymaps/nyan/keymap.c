e00945519971799e37ae4f20958d635628e6767e
Anh Nhân Nguyễn <anhnhan@anhnhan.de>
2025-10-07 02:10:34 +0200

    [gh60:nyan] disable lead key, add ins key

--- keyboards/gh60/revc/keymaps/nyan/keymap.c
+++ keyboards/gh60/revc/keymaps/nyan/keymap.c
@@ -51,14 +51,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_DEFAULT] = LAYOUT_all(
         KC_GESC,      KC_1,    KC_2,    KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,       KC_0,     KC_MINS,  TD(CT_EQL), TD(CT_BSLS),    KC_GRV, \
     LT(_FN2, KC_TAB), KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,     KC_Z,     KC_U,     KC_I,     KC_O,       KC_P, TD(CT_LBRC), TD(CT_RBRC),     KC_BSPC, \
-        MO(_FN),      KC_A,    KC_S,    KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L, TD(CT_CLN), TD(CT_QUOT),     KC_LEAD,   KC_SFTENT, \
+        MO(_FN),      KC_A,    KC_S,    KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L, TD(CT_CLN), TD(CT_QUOT),      ______,   KC_SFTENT, \
         KC_LSPO,   KC_LCTL,    KC_Y,    KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,     KC_SLSH,     KC_RSPC,   TG(_WASD), \
         KC_LCTL,   KC_LGUI, KC_LALT,                      KC_SPC,                                   KC_RALT, KC_RGUI,      KC_APP,      KC_RCTL),
 	[_FN] = LAYOUT_all(
         KC_GESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,   ______,   ______, \
         ______,  ARW_THN,    KC_UP,  ARW_THK,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______, \
         ______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_HOME,  KC_PGUP,  KC_PSCR,  KC_SLCK,  KC_PAUS,   ______,  DBL_CLN,   ______,   ______,   ______, \
-        ______,   ______,   ______,   ______,   KC_DEL,   KC_END,  KC_PGDN,   ______,   ______,   ______,   ______,   ______,   ______,   ______, \
+        ______,   ______,   ______,   KC_INS,   KC_DEL,   KC_END,  KC_PGDN,   ______,   ______,   ______,   ______,   ______,   ______,   ______, \
         ______,   ______,   ______,                       INDENT,                                           ______,   ______,   ______,   ______),
 	[_FN2] = LAYOUT_all(
         ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______, \
